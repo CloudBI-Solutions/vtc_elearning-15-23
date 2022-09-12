d5d26daf2ea5ffca9efb5ad07f5a827fefa2b1d4
Hiếu Kun <hieukun1011@gmail.com>
2022-09-12 05:37:55 -0700

    Create project elearning

--- addons_elearning/vtc_elearning/models/lecturers.py
+++ addons_elearning/vtc_elearning/models/lecturers.py
@@ -17,7 +17,30 @@ class Lecturers(models.Model):
     identity_card = fields.Char('Identity card')
     quiz_ids = fields.One2many('op.quiz', 'lecturers_id', string='Lecturers')
     evaluate_count = fields.Integer(string='Evaluate count', compute='comment_count', store=True)
+    total_course = fields.Integer('Total course', compute='calculate_count_course_student', store=True)
+    total_student = fields.Integer('Total student', compute='calculate_count_course_student', store=True)
+    rating = fields.Float('Rating', compute='calculate_rating', store=True)
+    rating_text = fields.Char('Rating')
 
+    @api.depends('comment_ids')
+    def calculate_rating(self):
+        for record in self:
+            if record.comment_ids:
+                rating = 0
+                total_rating = len(record.comment_ids)
+                for rec in record.comment_ids:
+                    rating += int(rec.rating)
+                record.rating = (rating/(total_rating * 5) * 5)
+                record.rating_text = str(round(record.rating, 1)) + '/5'
+
+    @api.depends('source_ids')
+    def calculate_count_course_student(self):
+        for record in self:
+            record.total_student = 0
+            course = self.env['slide.channel'].search([('lecturers_ids', 'in', [record.id])])
+            record.total_course = len(course)
+            for rec in course:
+                record.total_student += rec.count_student
     @api.depends('comment_ids')
     def comment_count(self):
         print('______')
--- addons_elearning/vtc_elearning/models/slide_slide.py
+++ addons_elearning/vtc_elearning/models/slide_slide.py
@@ -7,6 +7,18 @@ class SlideChannel(models.Model):
     lecturers_ids = fields.Many2many('lecturers', string='Lecturers')
     start_date = fields.Date('Start date')
     end_date = fields.Date('End date')
-    type_course = fields.Selection([('free', 'Free'), ('price', 'Price')], string='Type course')
+    type_course = fields.Selection([('free', 'Free'), ('price', 'Price')], string='Type course', default='free')
     comment_ids = fields.One2many('comment.course', 'course_id', string='Comment')
     state = fields.Selection([('draft', 'Draft'), ('confirm', 'Confirm'), ('expired', 'Expired')])
+    enroll = fields.Selection([
+        ('public', 'Public'), ('invite', 'On Invitation'), ('sign', 'Đăng ký (Coming soon)')],
+        default='public', string='Enroll Policy', required=True,
+        help='Condition to enroll: everyone, on invite, on payment (sale bridge).')
+    count_student = fields.Integer('Student count', compute='calculate_count_student', store=True)
+
+    @api.depends('student_ids')
+    def calculate_count_student(self):
+        for record in self:
+            if record.student_ids:
+                student = self.env['student.student'].search([('source_ids', 'in', [record.id])])
+                record.count_student = len(student)
