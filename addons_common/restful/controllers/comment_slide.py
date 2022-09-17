390dd9c0ec1017f8025eb2a6f960873859f2e845
Dương Trung Hiếu <hieukun1011@gmail.com>
2022-09-17 22:06:25 +0700

    update

---
+++ addons_common/restful/controllers/comment_slide.py
@@ -0,0 +1,92 @@
+import logging
+
+from odoo.addons.restful.common import (
+    valid_response,
+)
+from odoo.addons.restful.controllers.main import (
+    validate_token
+)
+
+from werkzeug import urls
+
+from addons_common.restful.common import invalid_response
+from odoo import http, SUPERUSER_ID
+from odoo.http import request
+
+_logger = logging.getLogger(__name__)
+
+
+class CommentSlide(http.Controller):
+
+    def get_url_base(self):
+        config = request.env['ir.config_parameter'].sudo().get_param('web.base.url')
+        if config:
+            return config
+        return 'https://test.diligo.vn:15000'
+
+    @http.route("/api/v1/comment/slide", type='http', auth="public", methods=["POST", "OPTIONS"], website=True,
+                csrf=False, cors="*")
+    def get_comment_slide_by_id(self, **payload):
+        values = []
+        base_url = CommentSlide.get_url_base(self)
+        print(payload.get('slide_id'))
+        comment1 = request.env['comment.slide'].sudo().search([('slide_id.id', '=', payload.get('slide_id'))])
+        print(comment1)
+        for i in comment1:
+            print(i.comment_ids)
+        list_comment = request.env['comment.slide'].sudo().search([('slide_id.id', '=', payload.get('slide_id')), ('comment_ids', '!=', False)])
+        # print(list_comment)
+        for record in list_comment:
+            data = {
+                'id': record.id,
+                'content': record.name,
+                'user_id': {'id': record.user_id.id, 'name': record.user_id.name, 'avatar': urls.url_join(base_url,
+                                            '/web/image?model=res.users&id={0}&field=image_1920'.format(
+                                                record.user_id.id))} or '',
+                'student': {'id': record.student_id.id, 'name': record.student_id.name, 'avatar': urls.url_join(base_url,
+                                                                                                          '/web/image?model=student.students&id={0}&field=avatar'.format(
+                                                                                                              record.student_id.id))} or '',
+                # 'content': record.name,
+            }
+            comments_below = []
+            for rec in record.comment_ids:
+                comment_ids = {
+                    'id': rec.id,
+                    'content': rec.name,
+                    'user_id': {'id': rec.user_id.id, 'name': rec.user_id.name, 'avatar': urls.url_join(base_url,
+                                                                                                              '/web/image?model=res.users&id={0}&field=image_1920'.format(
+                                                                                                                  record.user_id.id))} or '',
+                    'student': {'id': rec.student_id.id, 'name': rec.student_id.name,
+                                'avatar': urls.url_join(base_url,
+                                                        '/web/image?model=student.students&id={0}&field=avatar'.format(
+                                                            rec.student_id.id))} or '',
+                }
+                comments_below.append(comment_ids)
+                data['comments_below'] = comments_below
+
+
+            values.append(data)
+        return valid_response(values)
+
+    @http.route("/api/v1/add-comment-slide", type='http', auth="public", methods=["POST", "OPTIONS"], website=True,
+                csrf=False, cors="*")
+    def add_comment_slide_by_id(self, **payload):
+        field_require = [
+            'name',
+            'slide_id',
+            'student_id',
+            'comment_id',
+        ]
+        for field in field_require:
+            if field not in payload.keys():
+                return invalid_response(
+                    "Missing",
+                    "The parameter %s is missing!!!" % field)
+        domain = {
+            'name': payload.get('name'),
+            'slide_id': payload.get('slide_id'),
+            'student_id': int(payload.get('student_id')),
+            'comment_id': payload.get('comment_id'),
+        }
+        request.env['comment.slide'].with_user(SUPERUSER_ID).create(domain)
+        return invalid_response("Bạn đã ứng tuyển thành công vào vị trí %s." % payload.get('job_name'))
---
+++ addons_common/restful/controllers/sign_student.py
@@ -0,0 +1,60 @@
+import logging
+
+from odoo.addons.restful.common import (
+    valid_response,
+)
+from odoo.addons.restful.controllers.main import (
+    validate_token
+)
+
+from werkzeug import urls
+
+from addons_common.restful.common import invalid_response
+from odoo import http, SUPERUSER_ID
+from odoo.http import request
+
+_logger = logging.getLogger(__name__)
+
+
+class SignStudent(http.Controller):
+
+    @http.route("/api/v1/sign/student", type='http', auth="public", methods=["POST", "OPTIONS"], website=True,
+                csrf=False, cors="*")
+    def sign_student(self, **payload):
+        field_require = [
+            'login',
+            'password',
+        ]
+        for field in field_require:
+            if field not in payload.keys():
+                return invalid_response(
+                    "Missing",
+                    "The parameter %s is missing!!!" % field)
+        domain = {
+            'name': payload.get('login'),
+            'login': payload.get('login'),
+            'password': '1'
+        }
+        request.env['res.users'].with_user(SUPERUSER_ID).create(domain)
+        return invalid_response("Bạn đã ứng tuyển thành công vào vị trí %s." % payload.get('job_name'))
+
+    @http.route("/api/update-infor-student", type='http', auth="public", methods=["POST", "OPTIONS"], website=True,
+                csrf=False, cors="*")
+    def update_infor_student(self, **payload):
+        field_require = [
+            'login',
+            'password',
+        ]
+        for field in field_require:
+            if field not in payload.keys():
+                return invalid_response(
+                    "Missing",
+                    "The parameter %s is missing!!!" % field)
+        domain = {
+            'name': payload.get('login'),
+            'login': payload.get('login'),
+            'password': '1'
+        }
+        user = request.env['res.users'].with_user(SUPERUSER_ID).create(domain)
+        print(user)
+        return invalid_response("Bạn đã ứng tuyển thành công vào vị trí %s." % payload.get('job_name'))
