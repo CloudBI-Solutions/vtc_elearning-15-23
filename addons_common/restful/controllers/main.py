c59079d881a8410079f6c13af1eb93a9c2367d36
minhduc <phongnhiryan@gmail.com>
2022-09-17 17:09:25 +0700

    api fix bug

--- addons_common/restful/controllers/main.py
+++ addons_common/restful/controllers/main.py
@@ -18,8 +18,7 @@ def validate_token(func):
     @functools.wraps(func)
     def wrap(self, *args, **kwargs):
         """."""
-        headers = request.httprequest.headers
-        access_token = get_access_token(headers, kwargs)
+        access_token = kwargs.get('access_token')
         if not access_token:
             return invalid_response("access_token_not_found", "missing access token in request header", 401)
         access_token_data = (
