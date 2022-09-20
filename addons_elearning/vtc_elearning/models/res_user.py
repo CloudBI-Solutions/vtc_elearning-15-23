ccf26c91e7702a577adad8c1dac230a68a2f9aef
Dương Trung Hiếu <hieukun1011@gmail.com>
2022-09-20 23:01:33 +0700

    update

--- addons_elearning/vtc_elearning/models/res_user.py
+++ addons_elearning/vtc_elearning/models/res_user.py
@@ -6,7 +6,7 @@ class ResUsers(models.Model):
     res_country_state = fields.Many2one('res.country.state', string='Country state',
                                         domain="[('country_id', '=', 'VN')]")
     role_user = fields.Selection([('admin_operate', 'Admin Operate'),
-                                  ('admin_province', 'Admin admin_province'),
+                                  ('admin_province', 'Admin province'),
                                   ('user_operate', 'User Operate'),
                                   ('user', 'User')], string="Role user", default='user')
 
