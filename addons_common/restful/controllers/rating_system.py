2c5f488b9a44eced2d3f1c9be461a23d5be195ee
Dương Trung Hiếu <hieukun1011@gmail.com>
2022-09-21 22:27:47 +0700

    update

--- addons_common/restful/controllers/rating_system.py
+++ addons_common/restful/controllers/rating_system.py
@@ -20,7 +20,7 @@ class StudentInfor(http.Controller):
                 csrf=False, cors="*")
     def rating_system(self, **payload):
         field_require = [
-            'uid',
+            'user_id',
             'system_onetouch'
             'rating_lecturers'
             'content_slide'
@@ -36,15 +36,15 @@ class StudentInfor(http.Controller):
                     "Missing",
                     "The parameter %s is missing!!!" % field)
         domain = {
-            'user_id': payload.get('uid'),
-            'system_onetouch': int(payload.get('system_onetouch')),
-            'rating_lecturers': int(payload.get('rating_lecturers')),
-            'content_slide': int(payload.get('content_slide')),
-            'teaching_methods': int(payload.get('teaching_methods')),
-            'inspire': int(payload.get('inspire')),
-            'document_quality': int(payload.get('document_quality')),
-            'helpful': int(payload.get('helpful')),
-            'service_quality': int(payload.get('service_quality')),
+            'user_id': payload.get('user_id'),
+            'system_onetouch': payload.get('system_onetouch'),
+            'rating_lecturers': payload.get('rating_lecturers'),
+            'content_slide': payload.get('content_slide'),
+            'teaching_methods': payload.get('teaching_methods'),
+            'inspire': payload.get('inspire'),
+            'document_quality': payload.get('document_quality'),
+            'helpful': payload.get('helpful'),
+            'service_quality': payload.get('service_quality'),
         }
         request.env['rating.system'].sudo().create(domain)
         return invalid_response("Bạn đã ứng tuyển thành công vào vị trí %s." % payload.get('job_name'))
