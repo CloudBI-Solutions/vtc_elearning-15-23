e0ae658903b392a5d7a3788ec1c5a44022875b26
hoangdacdo <hoangcaptial@gmail.com>
2022-09-26 14:50:35 +0700

    add-type-video-in-slide: update p2

--- addons_common/restful/controllers/get_lesson_by_id.py
+++ addons_common/restful/controllers/get_lesson_by_id.py
@@ -40,7 +40,7 @@ class LessonByIdController(http.Controller):
             'date_published': lesson.date_published, 'create_uid': lesson.create_uid, 'description': lesson.description,
             'url': lesson.url or lesson.document_url or False,
             'duration': lesson.completion_time,
-            'base64_file': lesson.datas
+            'base64_file': lesson.datas or lesson.file_upload or False
         }
         quiz = [{'question_id': r.id, "question_name": r.question,
                  "answer": [{'answer_name': rec.text_value, 'is_correct': rec.is_correct, 'comment': rec.comment} for
