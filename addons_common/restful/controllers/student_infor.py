c1729895157658f60b298a659737e9544e05cb12
minhduc <phongnhiryan@gmail.com>
2022-09-28 14:02:04 +0700

    contrains-case

--- addons_common/restful/controllers/student_infor.py
+++ addons_common/restful/controllers/student_infor.py
@@ -1,10 +1,11 @@
 import logging
 
 from odoo.addons.restful.common import (
-    valid_response,
+	valid_response,
+	invalid_response
 )
 from odoo.addons.restful.controllers.main import (
-    validate_token
+	validate_token
 )
 from werkzeug import urls
 
@@ -16,36 +17,37 @@ _logger = logging.getLogger(__name__)
 
 class StudentInfor(http.Controller):
 
-    def get_url_base(self):
-        config = request.env['ir.config_parameter'].sudo().get_param('web.base.url')
-        if config:
-            return config
-        return 'https://test.diligo.vn:15000'
+	def get_url_base(self):
+		config = request.env['ir.config_parameter'].sudo().get_param('web.base.url')
+		if config:
+			return config
+		return 'https://test.diligo.vn:15000'
 
-    @validate_token
-    @http.route("/api/get/infor_student", type="http", auth="public", methods=["GET", "OPTIONS"], csrf=False, cors="*")
-    def get_infor_student_by_id(self, **payload):
-        values = []
-        base_url = StudentInfor.get_url_base(self)
-        user = request.env['res.users'].sudo().search([('id','=', int(payload.get('uid')))])
-        student = request.env['student.student'].sudo().search([('user_id', '=', user.id)])
-        # cấp độ học
-        datas = {'id': user.id,
-                 'name': user.name,
-                 'avatar': urls.url_join(base_url,
-                                         '/web/image?model=student.student&id={0}&field=avatar'.format(
-                                             student.id)),
-                 'birth': student.birth,
-                 'phone': student.phone,
-                 'email': student.email,
-                 'position': {'id': student.position.id, 'name': student.position.name} or '',
-                 'work_unit': student.work_unit,
-                 'res_country_state': {'id': student.res_country_state.id,
-                                       'name': student.res_country_state.name} or '',
-                 'res_country_ward': {'id': student.res_country_ward.id, 'name': student.res_country_ward.name} or '',
-                 'res_country_district': {'id': student.res_country_district.id,
-                                          'name': student.res_country_district.name} or '',
-                 }
-        values.append(datas)
-        print(values)
-        return valid_response(values)
+	@validate_token
+	@http.route("/api/get/infor_student", type="http", auth="public", methods=["GET", "OPTIONS"], csrf=False, cors="*")
+	def get_infor_student_by_id(self, **payload):
+		values = []
+		base_url = StudentInfor.get_url_base(self)
+		user = request.env['res.users'].sudo().search([('id', '=', int(payload.get('uid')))])
+		student = request.env['student.student'].sudo().search([('user_id', '=', user.id)])
+		if not student:
+			return invalid_response("Tạo tài khoản user cho thằng student này!")
+		# cấp độ học
+		datas = {'id': user.id,
+		         'name': user.name,
+		         'avatar': urls.url_join(base_url,
+		                                 '/web/image?model=student.student&id={0}&field=avatar'.format(
+			                                 student.id)),
+		         'birth': student.birth,
+		         'phone': student.phone,
+		         'email': student.email,
+		         'position': {'id': student.position.id, 'name': student.position.name} or '',
+		         'work_unit': student.work_unit,
+		         'res_country_state': {'id': student.res_country_state.id,
+		                               'name': student.res_country_state.name} or '',
+		         'res_country_ward': {'id': student.res_country_ward.id, 'name': student.res_country_ward.name} or '',
+		         'res_country_district': {'id': student.res_country_district.id,
+		                                  'name': student.res_country_district.name} or '',
+		         }
+		values.append(datas)
+		return valid_response(values)
