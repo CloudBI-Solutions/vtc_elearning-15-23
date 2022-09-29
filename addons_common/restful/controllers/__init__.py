519109e239eed5cc88570eed76c8a4294e004e27
Dương Trung Hiếu <hieukun1011@gmail.com>
2022-09-29 10:52:43 +0700

    update

--- addons_common/restful/controllers/__init__.py
+++ addons_common/restful/controllers/__init__.py
@@ -1,4 +1,6 @@
+from . import all_course
 from . import attachment
+from . import comment_slide
 # from . import announcement
 # from . import approvals
 # from . import attendance
@@ -6,20 +8,18 @@ from . import attachment
 # from . import device
 from . import elearning
 from . import employee
-# from . import helpdesk
-# from . import kpis
-from . import main, token
-# from . import project
-from . import quiz
-# from . import recruitment
-from . import user
-from . import token
-from . import all_course
 from . import get_course_by_id
 from . import get_lesson_by_id
-from . import sign_student
-from . import comment_slide
-from . import student_infor
-from . import res_country
+from . import helpdesk
+# from . import kpis
+from . import main, token
 from . import position_student
+# from . import project
+from . import quiz
 from . import rating_system
+from . import res_country
+from . import sign_student
+from . import student_infor
+from . import token
+# from . import recruitment
+from . import user
