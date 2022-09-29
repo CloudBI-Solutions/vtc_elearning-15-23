8b0ff11ba21e58987a89e6607bcc40ce370baabe
Dương Trung Hiếu <hieukun1011@gmail.com>
2022-09-29 11:13:09 +0700

    update

--- addons_common/restful/controllers/helpdesk.py
+++ addons_common/restful/controllers/helpdesk.py
@@ -26,7 +26,7 @@ class HelpdeskController(http.Controller):
         return 'https://test.diligo.vn:15000'
 
     @validate_token
-    @http.route("/api/v1/helpdesk/create_helpdesk", type="http", auth="public", methods=["POST"], csrf=False, cors='*')
+    @http.route("/api/v1/helpdesk/create_helpdesk", type="http", auth="public", methods=["POST", "OPTIONS"], csrf=False, cors='*')
     def post_helpdesk(self, **payload):
         print(request.uid)
         # user = request.env['res.users'].sudo().search([('id', '=', payload.get('uid'))])
@@ -56,7 +56,7 @@ class HelpdeskController(http.Controller):
         return invalid_response("ok")
 
     @validate_token
-    @http.route("/api/v1/type_maintenance_request", type="http", auth="public", methods=["GET"], csrf=False, cors='*')
+    @http.route("/api/v1/type_maintenance_request", type="http", auth="public", methods=["GET", "OPTIONS"], csrf=False, cors='*')
     def get_type_maintenance_request(self, **payload):
         values = []
         data = request.env['type.maintenance.request'].sudo().search([('type', '=', 'onetouch')])
